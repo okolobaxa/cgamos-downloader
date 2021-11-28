2161335597d4ec1c3c097ffbce938c3678b8dc9d
Anton Kheystver <okolobaxa@gmail.com>
2021-11-28 13:07:10 +0100

    Add support for jewish metric books

--- DataParser.cs
+++ DataParser.cs
@@ -20,10 +20,11 @@ namespace cgamos
                 var client = new HttpClient();
                 string url = record.Fond switch
                 {
-                    "203" when record.Opis == "747" => $"https://cgamos.ru/ispovedalnye_vedomosti/{record.Fond}/{record.Fond}-{record.Opis}/{record.Fond}-{record.Opis}-{record.Delo}/",
-                    "51" => $"https://cgamos.ru/skazki/{record.Fond}/{record.Fond}-{record.Opis}/{record.Fond}-{record.Opis}-{record.Delo}/",
-                    "2200" => $"https://cgamos.ru/inye-konfessii/islam/{record.Fond}/{record.Fond}-{record.Opis}/{record.Fond}-{record.Opis}-{record.Delo}/",
-                    "2125" => $"https://cgamos.ru/ispovedalnye_vedomosti/{record.Fond}/{record.Fond}-{record.Opis}/{record.Fond}-{record.Opis}-{record.Delo}/",
+                    "203" when record.Opis == "747" => $"https://cgamos.ru/ispovedalnye_vedomosti/{record.Fond}-{record.Opis}-{record.Delo}/",
+                    "51" => $"https://cgamos.ru/skazki/{record.Fond}-{record.Opis}-{record.Delo}/",
+                    "2200" => $"https://cgamos.ru/inye-konfessii/islam/{record.Fond}-{record.Opis}-{record.Delo}/",
+                    "2125" => $"https://cgamos.ru/ispovedalnye_vedomosti/{record.Fond}-{record.Opis}-{record.Delo}/",
+                    "2372" => $"https://cgamos.ru/inye-konfessii/iudaizm/{record.Fond}-{record.Opis}-{record.Delo}/",
                     _ when MetricBookFonds.Contains(record.Fond) => $"https://cgamos.ru/metric-books/{record.Fond}/{record.Fond}-{record.Opis}/{record.Fond}_{record.Opis}_{record.Delo}/",
                     _ => throw new ArgumentOutOfRangeException(record.Fond)
                 };
